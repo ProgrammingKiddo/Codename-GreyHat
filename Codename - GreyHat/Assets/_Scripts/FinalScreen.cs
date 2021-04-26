691a759d5e454f42c9626a1d35100e9d48d3d495
ProgrammingKiddo <programming.edu@protonmail.com>
2021-04-26 14:09:43 +0200

    Moved the game files to the repository after the jam was finished.

---
+++ Codename - GreyHat/Assets/_Scripts/FinalScreen.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Copyright (c) Borja Fernández
+ *
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalScreen : MonoBehaviour
+{
+
+    #region Variables
+    public GameObject goodEnding;
+    public GameObject badEnding;
+    public GameObject worseEnding;
+    #endregion
+
+
+    #region UnityMethods
+
+    void Start()
+    {
+        switch(PlayerPrefs.GetInt("Final"))
+        {
+            case -1:
+                worseEnding.SetActive(true);
+                break;
+            case 0:
+                badEnding.SetActive(true);
+                break;
+            case 1:
+                goodEnding.SetActive(true);
+                break;
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+
+    #endregion
+}
